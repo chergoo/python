5130a9af556261f982dcb018e5d7d3e9af7f9c84
cee <chgo.nn22@gmail.com>
2024-06-21 17:32:34 +0800

    xlsx表格生成时间线图

---
+++ LOGG/LOGGER.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+
+
+# 设置中文字体
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体为中文宋体
+
+# 读取Excel文件
+df = pd.read_excel("log.xlsx")
+
+# 将日期转换为datetime格式
+df['时间'] = pd.to_datetime(df['时间'], format='%Y%m%d')
+
+# 提取事件时间和名称
+times = df['时间'].tolist()
+names = df['项目'].tolist()
+
+# 创建图形和轴
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# 绘制时间线
+ax.plot(times, [1]*len(times), marker='o', linestyle='-', color='b')
+
+# 为每个事件添加标签
+for i, (time, name) in enumerate(zip(times, names)):
+    ax.text(time, 1, name, rotation=45, ha='right', va='bottom')
+
+# 设置日期格式
+# ax.xaxis.set_major_locator(mdates.FixedLocator(times))  # 设置X轴主刻度为数据中的日期
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+ax.xaxis.set_major_locator(mdates.DayLocator())
+
+# 设置X轴的范围为数据中的最小日期到最大日期
+ax.set_xlim(min(times), max(times))
+
+# 设置只显示数据中的日期刻度
+ax.set_xticks(times)
+
+# 设置图形的格式和标题
+plt.title("Project Timeline")
+plt.xlabel("Time")
+plt.yticks([])  # 隐藏y轴刻度
+plt.grid(True)
+plt.tight_layout()
+
+# 显示图形
+plt.show()
+
